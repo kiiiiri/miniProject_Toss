e2899d259b1bb27ef2031cfc9658a5331a88651d
SeungHun333 <yui332@naver.com>
2025-02-18 11:14:31 +0900

    pagination javaScript 구현

--- announcement/js/index.js
+++ announcement/js/index.js
@@ -1,32 +1,64 @@
 $(document).ready(async function() {
-  try {
-    let response = await fetch("/data/announce.json"); 
-    let jsonData = await response.json();
+  let pageNumber = 0;
 
-    console.log("JSON 데이터 확인:", jsonData); 
+  let response = await fetch("/data/announce.json"); 
+  let jsonData = await response.json();
 
-    $(".notice-list").empty();
+  console.log("JSON 데이터 확인:", jsonData); 
 
-    if (!jsonData.contets || !Array.isArray(jsonData.contets)) {
-      console.error("contets 배열이 존재하지 않음:", jsonData);
-      return;
+  $(".notice-list").empty();
+
+  console.log(jsonData.count);
+  for (let i = 1; i <= jsonData.count; i++) {
+    if (i === 1) {
+      $(".pagination").append(`<li class="prev"><button><</button></li>`)
     }
-    $.each(jsonData.contets, function(index, item) {
-        console.log("item 확인:", item); 
 
+    $(".pagination").append(`<li class=${i === 1 ? 'active' : ''}><button class="btn page_btn">${i}</button></li>`)
+
+    if (i === jsonData.count) {
+      $(".pagination").append(`<li class="next"><button>></button></li>`)
+    }
+  }
+
+  applyContentData(1);
+
+  $(".page_btn").on("click", function() {
+    $(".notice-list").empty();
+    pageNumber = $(this).text();
+
+    $(".pagination li").removeClass("active");
+    $(this).parent().addClass("active");
+
+    applyContentData(pageNumber);
+  })
+
+  function applyContentData(pageNumber) {
+    let pageKey = `page${pageNumber}`;
+
+    $.each(jsonData[pageKey].contents, function(index, content) {
+      console.log("content 확인:", content);
+      if (index == 9) {
         $(".notice-list").append(`
-            <li> 
-                <a href="${item.url}">
-                    <h4 class="notice-content">${item.title}</h4>
-                    <p class="date">${item.date}</p>
-                    <hr>
-                </a>
-            </li>
-        `);
+          <li class="active"> 
+              <a href="${content.url}">
+                  <h4 class="notice-content">${content.title}</h4>
+                  <p class="date">${content.date}</p>
+              </a>
+          </li>
+      `);
+      } else {
+        $(".notice-list").append(`
+          <li> 
+              <a href="${content.url}">
+                  <h4 class="notice-content">${content.title}</h4>
+                  <p class="date">${content.date}</p>
+              </a>
+              <hr>
+          </li>
+      `);
+      }  
     });
-
-  } catch (error) {
-    console.error("JSON 데이터를 불러오는 중 오류 발생:", error);
   }
 });
 
